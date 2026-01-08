63752ae361dd4662ebb865d078c338bb67aa1fe2
Andrew C Leacock <Andrewcleacock@gmail.com>
2026-01-08 14:46:06 -0500

    clean up +

---
+++ viq/simulation/__init__.py
@@ -0,0 +1,4 @@
+from daily_demand import DailyDemand
+from asset_sim import AssetSimulator
+
+__all__ = ["DailyDemand", "AssetSimulator"]
---
+++ viq/simulation/daily_demand.py
@@ -0,0 +1,30 @@
+import math
+import random
+
+'''Module to simulate daily demand using a Poisson distribution.'''
+
+
+class DailyDemand:
+    def __init__(self, average_daily_sales):
+        self.average_daily_sales = average_daily_sales
+
+    def _units_sold_(self):
+        cutoff_value = math.exp(-self.average_daily_sales)
+        units_sold_today = 0
+        random_product = 1.0
+
+        while random_product > cutoff_value:
+            units_sold_today += 1
+            random_product *= random.random()
+
+        return units_sold_today - 1
+
+
+if __name__ == "__main__":
+    daily_demand = DailyDemand(average_daily_sales=2.6)
+    print([daily_demand._units_sold_() for _ in range(365)])
+
+
+
+
+
