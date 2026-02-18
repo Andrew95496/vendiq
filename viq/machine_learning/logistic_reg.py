124302b9ad9179a62cfb80e780346cae2d0d2ec2
Andrew  Leacock <Andrewcleacock@gmail.com>
2026-02-18 11:46:40 -0500

    Implement stochastic gradient descent for logistic regression by updating weights and bias using single-sample gradients instead of full-batch averaging

--- viq/machine_learning/logistic_reg.py
+++ viq/machine_learning/logistic_reg.py
@@ -1,61 +1,40 @@
 import numpy as np
-import pandas as pd
-import plotly.express as px
+X = np.random.randint(0, 100, size=10)
+y = np.random.randint(0, 100, size=10)
 
-# Load data
-df = pd.read_excel("/Users/andrewleacock1/Downloads/log.xlsx")
 
-# ----- Define Over-Serviced -----
-sales_threshold = 70
-y = (df["Avg Qty Sold per Visit"] < sales_threshold).astype(int).values
-
-# Feature
-X_raw = df["Avg Outs per Visit"].values
-
-# Scale feature
-X_mean = X_raw.mean()
-X_std = X_raw.std()
-X = (X_raw - X_mean) / X_std
-
-# ----- Train Logistic Regression -----
-w = 0.0
-b = 0.0
-lr = 0.01
-epochs = 5000
+w = np.random.normal()
+b = np.random.normal()
+lr = 0.000001
 n = len(X)
 
-for _ in range(epochs):
-    z = w * X + b
-    p = 1 / (1 + np.exp(-z))
 
-    dw = (1/n) * np.sum((p - y) * X)
-    db = (1/n) * np.sum(p - y)
+# Stochastic gradient descent
+
+for _ in range(n):
+    i = np.random.randint(n)
+    z = w * X[i] + b
+    lg = 1 / (1 + np.exp(-z))
+
+    dw = (lg - y[i]) * X[i]
+    db = (lg - y[i])
 
     w -= lr * dw
     b -= lr * db
+    print(f"Stochastic: {'weight(s):':<15}{w:>10.6f} | {'bias:':<6}{b:>10.6f}")
 
-print("Best w:", w)
-print("Best b:", b)
 
-# ----- Graph -----
-x_range = np.linspace(X_raw.min(), X_raw.max(), 1000)
-x_scaled = (x_range - X_mean) / X_std
+#  Full batch gradient descent
 
-z = w * x_scaled + b
-prob_curve = 1 / (1 + np.exp(-z))
+for _ in range(n):
+    z = w * X + b
+    lg = 1 / (1 + np.exp(-z))
 
-fig = px.scatter(
-    x=X_raw,
-    y=y,
-    labels={"x": "Avg Outs per Visit", "y": "Over-Serviced (0/1)"},
-    title="Probability Machine Is Over-Serviced"
-)
+    dw = (1/n) * np.sum((lg - y) * X)
+    db = (1/n) * np.sum(lg - y)
 
-fig.add_scatter(
-    x=x_range,
-    y=prob_curve,
-    mode="lines",
-    name="Logistic Curve"
-)
+    w -= lr * dw
+    b -= lr * db
+    print(f"Full Batch: {'weight(s):':<15}{w:>10.6f} | {'bias:':<6}{b:>10.6f}")
 
-fig.show()
+
